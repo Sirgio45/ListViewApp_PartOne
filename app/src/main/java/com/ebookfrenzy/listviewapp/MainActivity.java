66f98a7557ac1144f275065722c360c8ed50df89
Sirgio45 <15419260@mstc.edu>
2016-11-12 11:58:57 -0600

    Initial commit

---
+++ app/src/main/java/com/ebookfrenzy/listviewapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.ebookfrenzy.listviewapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Simple array with a list of my favorite TV shows
+        String[] favoriteTVShows = {"Pushing Daisies", "Better Off Ted",
+                "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
+                "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
+        // The ListAdapter acts as a bridge between the data and each ListItem
+        // You fill the ListView with a ListAdapter. You pass it a context represented by
+        // this. A Context provides access to resources you need.
+        // android.R.layout.simple_list_item_1 is one of the resources needed.
+        // It is a predefined layout provided by Android that stands in as a default
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                favoriteTVShows);
+        // ListViews display data in a scrollable list
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+        // Tells the ListView what data to use
+        theListView.setAdapter(theAdapter);
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String tvShowPicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(i));
+                Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }}
